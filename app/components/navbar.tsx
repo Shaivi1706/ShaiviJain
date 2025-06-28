0134a184e96ba9ab53618b617c2625f8bde77d7f
Shaivi Jain <shaivijainn@gmail.com>
2025-06-29 00:10:28 +0530

    final i hope

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -48,7 +48,6 @@ export function NavbarDemo() {
 
           <MobileNavMenu
             isOpen={isMobileMenuOpen}
-            onClose={() => setIsMobileMenuOpen(false)}
           >
             {navItems.map((item, idx) => (
               <a
